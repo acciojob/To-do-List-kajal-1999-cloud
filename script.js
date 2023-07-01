c769732e572b3e1b1ae93236a7fd323f3e283096
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-01 08:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+// getting the refernce using id 
+const todoInput = document.getElementById("newTodoInput") ;
+const todoBtn = document.getElementById("addTodoBtn") ;
+const todoList = document.getElementById("todoList") ;
 
+// take input on click of button
+todoBtn.addEventListener(click, () =>{
+	// taking input from the user
+const todoItem = todoInput.value.trim() ;
+	// if string is not empty
+	if(todoItem !== ''){
+		// creating an element
+		const todoText = document.createElement("li") ;
+		
+		todoText.textContent = todoItem ;
+	}
+	todoList.appendChild(todoText) ;
+// creating space for ne Inout
+	todoInput = '';
+	
+});
