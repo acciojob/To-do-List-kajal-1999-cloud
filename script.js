1f37541dde49120494d5aec84bd99c04d649350e
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-01 14:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-//your code here
-// getting the refernce using id 
-const todoInput = document.getElementById("newTodoInput") ;
-const todoBtn = document.getElementById("addTodoBtn") ;
-const todoList = document.getElementById("todoList") ;
+// // Get references to the input field, button, and todo list
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
 
-// take input on click of button
-todoBtn.addEventListener(click, () =>{
-	// taking input from the user
-const todoItem = todoInput.value.trim() ;
-	// if string is not empty
-	if(todoItem !== ''){
-		// creating an element
-		const todoText = document.createElement("li") ;
-		
-		todoText.textContent = todoItem ;
-	}
-	todoList.appendChild(todoText) ;
-// creating space for ne Inout
-	todoInput = '';
-	
+// Function to add a new todo item
+function addTodo() {
+  // Get the value from the input field
+  const todoText = newTodoInput.value.trim();
+
+  // Validate if the todo item is not empty
+  if (todoText !== "") {
+    // Create a new list item
+    const newTodoItem = document.createElement("li");
+    newTodoItem.textContent = todoText;
+
+    // Append the new item to the todo list
+    todoList.appendChild(newTodoItem);
+
+    // Clear the input field
+    newTodoInput.value = "";
+  }
+}
+
+// Event listener for the add todo button click
+addTodoBtn.addEventListener("click", addTodo);
+
+// Event listener for the enter key press in the input field
+newTodoInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTodo();
+  }
 });
